81f312e5aae6200e1a67ec7a9ec626866cb97882
PC <electroniqueccc@gmail.com>
2018-06-03 19:36:12 +0100

    a

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(8);
     }
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void Button1(View view){
+        displayForTeamA(3);
+    }
+    public void Button2(View view){
+        displayForTeamA(2);
+    }
+    public void Button3(View view){
+        displayForTeamA(1);
+    }
+
 }
